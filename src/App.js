3861d9b551ee2684cea6edcaff99f5f8a5975a1b
Ridho Abdul Majid <ridhoabdulmajid@gmail.com>
2020-09-07 08:50:29 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ProductContext from './ProductContext';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
+import ProductList from './ProductList';
+
 
 function App() {
+  state = {
+    products : [
+      {id: 1, name: "Lemari", stock:20, price: 2999999},
+      {id: 2, name: "Meja", stock:30, price: 4999999},
+      {id: 3, name: "Kursi", stock:50, price: 1999999}
+  ],
+  cart: []
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProductContext.Provider 
+      value ={
+        {products : state.products},
+        {cart: state.cart}
+      }
+    >
+
+    </ProductContext.Provider>
   );
 }
 
